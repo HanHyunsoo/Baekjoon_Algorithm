c115899d85f518e195acaa1f5355807f2ce08a15
HanHyunsoo <gustn4563@gmail.com>
2018-08-11 01:47:04 +0900

    The second largest number

---
+++ Python/10817.py
@@ -0,0 +1,15 @@
+a,b,c=map(int, input().split())
+if b < a < c or c < a < b:
+    print(a)
+elif a < b < c or c < b < a:
+    print(b)
+elif a < c < b or b < c < a:
+    print(c)
+elif a==b:
+    print(a)
+elif b==c:
+    print(b)
+elif c==a:
+    print(c)
+else:
+    print(a)
