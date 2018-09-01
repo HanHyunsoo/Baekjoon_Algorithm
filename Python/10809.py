4b0877e1cc0907731e703304d6f8dde9ac64f8ad
HanHyunsoo <gustn4563@gmail.com>
2018-09-01 14:44:28 +0900

    Repeat string

---
+++ Python/10809.py
@@ -0,0 +1,9 @@
+a=[chr(i) for i in range(97,123)]
+S=list(input())
+result=""
+for i in a:
+    if S.count(i) > 0:
+        result += str(S.index(i)) + " "
+    else:
+        result += "-1 "
+print(result)
