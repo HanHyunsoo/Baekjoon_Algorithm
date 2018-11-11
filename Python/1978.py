54375af235957a42325598082fc2f1eaa62b349c
HanHyunsoo <gustn4563@gmail.com>
2018-11-12 00:43:46 +0900

    Find a prime number

--- Python/1978.py
+++ Python/1978.py
@@ -2,10 +2,10 @@ n = int(input())
 numbers = list(map(int ,input().split()))
 count = 0
 for i in range(n):
-    test = []
+    case = []
     for j in range(1, numbers[i] + 1):
         if numbers[i] % j == 0:
-            test.append(j)
-    if len(test) == 2:
+            case.append(j)
+    if len(case) == 2:
         count += 1
 print(count)
