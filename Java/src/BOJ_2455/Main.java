8e0cf6427c1442b090a9033e2cc8b978ee04fce0
HanHyunsoo <gustn4563@gmail.com>
2019-03-16 02:28:03 +0900

    Intelligent train

---
+++ Java/src/BOJ_2455/Main.java
@@ -0,0 +1,26 @@
+package BOJ_2455;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int STATION = 4;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int case_people = 0;
+        
+        for(int i = 0; i < STATION; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int out_train = Integer.parseInt(st.nextToken());
+            int in_train = Integer.parseInt(st.nextToken());
+            case_people += in_train - out_train;
+
+            if(max < case_people) {
+                max = case_people;
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
