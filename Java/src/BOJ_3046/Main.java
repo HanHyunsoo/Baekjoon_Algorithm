9a8b0a8a392bb2241b3a12ad5df78203dd18bbae
HanHyunsoo <gustn4563@gmail.com>
2019-03-23 15:46:43 +0900

    R2

---
+++ Java/src/BOJ_3046/Main.java
@@ -0,0 +1,19 @@
+package BOJ_3046;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int R2 = 2 * S - R1;
+
+        System.out.println(R2);
+    }
+}
