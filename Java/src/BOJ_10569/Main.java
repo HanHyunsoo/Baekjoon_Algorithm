fbbc002db1e398dc188907860bd4114a1a29adc9
HanHyunSoo <gustn4563@gmail.com>
2019-04-13 03:23:46 +0900

    Polyhedron

---
+++ Java/src/BOJ_10569/Main.java
@@ -0,0 +1,25 @@
+package BOJ_10569;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            bw.write(2 - V + E + "\n");
+            bw.flush();
+        }
+        
+        br.close();
+        bw.close();
+    }
+}
