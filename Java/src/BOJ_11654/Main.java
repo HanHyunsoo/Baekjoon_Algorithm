76bb9c195855c40285264ae315c51ea17fa99a9e
HanHyunSoo <gustn4563@gmail.com>
2019-04-18 23:18:21 +0900

    ASCII

---
+++ Java/src/BOJ_11654/Main.java
@@ -0,0 +1,11 @@
+package BOJ_11654;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+        System.out.println(inputString.charAt(0) - 0);
+    }
+}
