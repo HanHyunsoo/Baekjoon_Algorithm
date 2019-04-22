13410852c8610c167da301a599aad2f6542b9bb7
HanHyunSoo <gustn4563@gmail.com>
2019-04-22 19:13:23 +0900

    Sum of Odd Sequence

--- Python/9713.py
+++ Python/9713.py
@@ -1,4 +1,7 @@
 result = 0
 for i in range(int(input())):
-    result += int(input())
+    result = 0
+    for j in range(1, int(input()) + 1):
+        if j % 2 == 1:
+            result += j
     print(result)
