3b808896cec9016ea192192038eeebb785aebeda
hanhyunsoo <gustn4563@gmail.com>
2021-12-18 21:48:51 +0900

    3053번 - 택시 기하학

---
+++ Java/src/BOJ_3053/Main.java
@@ -0,0 +1,14 @@
+package BOJ_3053;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+
+        System.out.println(r * r * Math.PI);
+        System.out.println(2 * r * r);
+    }
+}
