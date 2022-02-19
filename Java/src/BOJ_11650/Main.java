31c9aceb08f10efcf762ae7ea9c288c53d0aa62b
hanhyunsoo <gustn4563@gmail.com>
2022-02-20 05:35:36 +0900

    11650번 - 좌표 정렬하기

---
+++ Java/src/BOJ_11650/Main.java
@@ -0,0 +1,38 @@
+package BOJ_11650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(matrix, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream(matrix).forEach(x -> sb.append(x[0]).append(' ').append(x[1]).append('\n'));
+
+        System.out.println(sb);
+    }
+}
