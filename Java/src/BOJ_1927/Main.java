ca8ce21e78593e7e98377cea5432f34ec74ea469
hanhyunsoo <gustn4563@gmail.com>
2022-03-19 22:22:14 +0900

    1927번 - 최소 힙

---
+++ Java/src/BOJ_1927/Main.java
@@ -0,0 +1,30 @@
+package BOJ_1927;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+
+            if (temp == 0) {
+                if (priorityQueue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(priorityQueue.poll());
+                }
+            } else {
+                priorityQueue.add(temp);
+            }
+        }
+    }
+}
