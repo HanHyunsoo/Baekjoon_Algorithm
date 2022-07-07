711c2760a0c62d78e3ab91e30cfc1fcc3c5aef78
HanHyunSoo <gustn4563@gmail.com>
2022-07-07 15:12:36 +0900

    5717번 - 상근이의 친구들

---
+++ Java/src/BOJ_5717/Main.java
@@ -0,0 +1,15 @@
+package BOJ_5717;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M, F;
+
+        while ((M = sc.nextInt()) != 0 && (F= sc.nextInt()) != 0) {
+            System.out.println(M + F);
+        }
+    }
+}
