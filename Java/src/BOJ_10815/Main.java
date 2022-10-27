5b8a0d8cafdf25913851e079dd2582ad89b8280e
HanHyunSoo <gustn4563@gmail.com>
2022-10-27 13:58:17 +0900

    refactor : 10815번 퀵소트 구현

--- Java/src/BOJ_10815/Main.java
+++ Java/src/BOJ_10815/Main.java
@@ -11,8 +11,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(numbers);
+        int[] numbers = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        quickSort(numbers, 0, numbers.length - 1);
 
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,5 +31,31 @@ public class Main {
         }
 
         System.out.println(sb);
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        int part = partition(arr, start, end);
+        if (start < part - 1) quickSort(arr, start, part - 1);
+        if (end > part) quickSort(arr, part, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+        while (start <= end) {
+            while (arr[start] < pivot) start++;
+            while (arr[end] > pivot) end--;
+            if (start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr, int start, int end) {
+        int tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
     }
 }
