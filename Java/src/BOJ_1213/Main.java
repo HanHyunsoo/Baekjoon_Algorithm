1291390c222fcf3d2e94e74031558c3f1919ac0e
HanHyunSoo <gustn4563@gmail.com>
2023-01-02 15:27:36 +0900

    1213번 - 팰린드롬 만들기

--- Java/src/BOJ_1213/Main.java
+++ Java/src/BOJ_1213/Main.java
@@ -1,29 +1,32 @@
 package BOJ_1213;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        int[] alphabets = new int[26];
+        String input = br.readLine();
+
+        for (char ch : input.toCharArray()) {
+            alphabets[ch - 'A']++;
+        }
+
         StringBuilder sb = new StringBuilder();
         String middle = "";
 
-        int[] alphabet = new int[26];
-
-        str.chars().forEach(x -> alphabet[x - 'A']++);
-
-        for (int i = 0; i < alphabet.length; i++) {
-            sb.append(String.valueOf((char) (i + 'A')).repeat(alphabet[i] / 2));
-            if (alphabet[i] % 2 == 1) {
-                middle = String.valueOf((char) (i + 'A'));
-            }
+        for (int i = 0; i < 26; i++) {
+            sb.append(String.valueOf((char) (i + 'A')).repeat(alphabets[i] / 2));
+            if (alphabets[i] % 2 == 1) middle = String.valueOf((char) (i + 'A'));
         }
 
-        String result = sb + middle;
-        result += sb.reverse();
+        String result = sb.toString();
+        result += middle + sb.reverse();
 
-        System.out.println((result.length() == str.length()) ? result : "I'm Sorry Hansoo");
+        System.out.println((result.length() == input.length()) ? result : "I'm Sorry Hansoo");
     }
+
 }
